fb017abd70df1b33e2b936891dc5e86697a250ba
Shawn Webb <lattera@gmail.com>
2011-12-23 12:52:53 -0700

    Add start and end ip arguments

--- freeip.sh
+++ freeip.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 subnet="$1"
+startip="$2"
+endip="$3"
 i=0
 
-for i in `seq 2 254`;
+for i in `seq $startip $endip`;
 do
     ping -c1 $subnet.$i > /dev/null 2>&1
     if [ $? -gt 0 ]; then
