0080392ab29cf3e1d3bd2601ef5718cf60376c88
Shawn Webb <lattera@gmail.com>
2012-02-27 14:04:43 -0700

    libtool needs to be installed. Fix referencing the right files.

--- portbuild.sh
+++ portbuild.sh
@@ -4,7 +4,7 @@ date=`/bin/date '+%F_%T'`
 curdir="$(cd "$(dirname $0)" && pwd)"
 
 # Step 1 - Sanity checking
-if [ ! -f "pkgs.txt" ]; then
+if [ ! -f "${curdir}/pkgs.txt" ]; then
     echo "Please enter packages you would like to build in pkgs.txt"
     exit 1
 fi
@@ -17,21 +17,32 @@ if [ $# -gt 0 ]; then
     done
 fi
 
-if [ -f "pkgexcludes.txt" ]; then
-    for exclude in `cat pkgexcludes.txt`; do
+if [ -f "${curdir}/pkgexcludes.txt" ]; then
+    for exclude in `cat ${curdir}/pkgexcludes.txt`; do
         excludes="${excludes} | grep -v ${exclude}"
     done
 fi
 
+# Step 4 - Set up initial logging
 exec 1> $curdir/logs/$date
 exec 2>&1
 
-# Step 4 - Delete all packages minus the above excluded packages
+# Step 5 - Delete all packages minus the above excluded packages
 cmd="pkg_info | awk '{print \$1;}' ${excludes} | xargs sudo pkg_delete"
 eval $cmd
 
-# Step 5 - Build new packages
-for pkg in `cat pkgs.txt`; do
+# Step 6 - Make sure libtool is installed
+pkg_info | grep libtool > /dev/null
+
+if [ $? -gt 0 ]; then
+    echo "==== Installing libtool ===="
+
+    cd /usr/ports/devel/libtool
+    sudo make DISABLE_LICENSES=YES BATCH=YES package-recursive clean
+fi
+
+# Step 7 - Build new packages
+for pkg in `cat ${curdir}/pkgs.txt`; do
     logfile=`echo $pkg | sed 's/\//_/g'`
     exec 1> $curdir/logs/$date-$logfile
     exec 2>&1
